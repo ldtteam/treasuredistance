4b581300199f260a74ce2288e1b8fb1c61ef3e15
Raycoms <ray.neiheiser@gmail.com>
2023-06-12 18:14:29 +0200

    1.20

---
+++ src/main/java/com/ldtteam/treasuredistance/TreasureDistance.java
@@ -0,0 +1,12 @@
+package com.ldtteam.treasuredistance;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("treasuredistance")
+public class TreasureDistance
+{
+    public TreasureDistance()
+    {
+       // Noop
+    }
+}
